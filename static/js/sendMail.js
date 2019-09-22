f843f23eeb15e5c531bfd0f4bdbe6bc0bec62a58
Ubuntu <ubuntu@ip-172-31-30-14.ec2.internal>
2019-09-22 22:47:15 +0000

    adjusted images for better display on all pages. Changed footer to 3 links. Wired up modals for footer links

--- static/js/sendMail.js
+++ static/js/sendMail.js
@@ -1,12 +1,14 @@
 function sendMail(contactForm) {
-    var service_id = "default_service";
-    var template_id = "Pet-sitting";
+    var service_id = "priority_pets";
+    var template_id = "pet_stting_request";
     
     var template_params= {
-        "from_first_name": contactForm.firstname.value,
-        "from_last_name": contactForm.lastname.value,
-        "from_email": contactForm.emailaddress.value,
-        "petsitting_request": contactForm.petsittingrequest.value
+        "subject": contactForm.subject.value,
+        "petsitting_request": contactForm.petsittingrequest.value,
+        "firstname": contactForm.firstname.value,
+        "lastname": contactForm.lastname.value,
+        "emailaddress": contactForm.emailaddress.value
+        
     }
 
     emailjs.send(service_id, template_id, template_params)
