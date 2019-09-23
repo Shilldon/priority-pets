2cf31d3cf3b44db0cbf5a0d229c8884f11540f0c
Ubuntu <ubuntu@ip-172-31-30-14.ec2.internal>
2019-09-23 12:13:14 +0000

    changed appearance of thumbnails on mobile device on the_paddock. Created and tested feedback modal for submitting contact form

--- static/js/sendMail.js
+++ static/js/sendMail.js
@@ -14,10 +14,17 @@ function sendMail(contactForm) {
     emailjs.send(service_id, template_id, template_params)
     .then(
         function(response){
-            console.log("SUCCESS", response)
+            console.log("SUCCESS", response);
+            $("#modal-message-text").text('');
+            $("#modal-message-text").append("Thanks for your message. I will be in touch shortly.");
+            $("#message-modal").modal('show');
+            $('#contact-form').find(":input").val('');
         },
         function(error) {
-            console.log("FAILED", error)
+            $("#modal-message-text").text('');
+            $("#modal-message-text").append("There was an error sending your message. <br/>Please try again later, or call me on 07387 715 779.");
+            $("#message-modal").modal('show');      
+            console.log("FAILED", error);
         }
     );
     return false;
